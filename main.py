dec26c6699ab5a1c4b39d05f690d85d76417c614
GrumpiestDonut <grumpydonut@proton.me>
2025-02-09 10:58:27 -0600

    saving work in progress

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+def myfunction(file_contents):
+    my_string = file_contents
+    lowercase_string = my_string.lower()
+    count_a = lowercase_string.count('a')
+    print(count_a)
+
 def main():
     with open("/home/grumpydonut/workspace/github.com/GrumpiestDonut/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+        words = file_contents.split()
+        word_count = len(words)
+    print(word_count)
+    myfunction()
 
 
 if __name__ == "__main__":
