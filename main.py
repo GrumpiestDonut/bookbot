26efc8023d8a459e0063f615ffd31b624c1406f5
GrumpiestDonut <grumpydonut@proton.me>
2025-02-09 12:08:44 -0600

    finished the project

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-def myfunction(file_contents):
-    my_string = file_contents
-    lowercase_string = my_string.lower()
-    count_a = lowercase_string.count('a')
-    print(count_a)
+def char_counter(file_contents):
+    lowercase_string = file_contents.lower()
+    chardictcount = {}
+    for char in lowercase_string:
+        chardictcount[char] = chardictcount.get(char, 0) + 1
+    return chardictcount
 
 def main():
-    with open("/home/grumpydonut/workspace/github.com/GrumpiestDonut/bookbot/books/frankenstein.txt") as f:
+    bookpath = "books/frankenstein.txt"
+    with open(bookpath) as f:
         file_contents = f.read()
         words = file_contents.split()
         word_count = len(words)
-    print(word_count)
-    myfunction()
+    print(f"--- Begin report of {bookpath} ---")
+    print(f"{word_count} words found in the document\n")
+    chardictcount = char_counter(file_contents)
+    # print(chardictcount) 
+    for char, count in chardictcount.items():
+       if char.isalpha():
+    #       print(f"The SPACE character was found {count} times")
+    #    else:
+          print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
+
 
 
 if __name__ == "__main__":
